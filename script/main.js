00cd5814550aa43993a134f3019f50d939057106
sumin <sumin.designer@gmail.com>
2024-05-17 11:33:51 +0900

    파일추가

--- script/main.js
+++ script/main.js
@@ -1,3 +1,77 @@
+
+//-------------헤더 부분--------------
+
+//------------헤더 좌우스크롤시 보여지게 하기----------
+$(document).ready(function(){
+
+  //새로고침시 헤더위치 변경
+  let bwLeft = $(document).scrollLeft();
+  $('.h_inner').css('left',`-${bwLeft}px`)
+
+  //스크롤 할때마다 자동으로 헤더위치 변경
+  $(window).scroll(function(){
+    bwLeft = $(document).scrollLeft();
+    $('.h_inner').css('left',`-${bwLeft}px`)
+  })
+
+})
+
+// 헤더 메뉴 언어 설정버튼
+
+  //select를 누르면 active를 추가해주는 함수
+  function toggleSelectBox(selectBox) {
+    selectBox.classList.toggle("active");
+  }
+
+  //select클릭이벤트와 active 함수 연결
+  const selectBoxElements = document.querySelectorAll(".select");
+
+  selectBoxElements.forEach(selectBoxElement => {
+    selectBoxElement.addEventListener("click", function (e){
+      const targetElement = e.target;
+      const isOptionElement = targetElement.classList.contains("option");
+  
+      if(isOptionElement){
+        selectOption(targetElement);
+      }
+      
+      toggleSelectBox(selectBoxElement);
+    });
+  });
+
+  //option선택시 해당값 선택
+  function selectOption(optionElement) {
+    const selectBox = optionElement.closest(".select");
+    const selectedElement = selectBox.querySelector(".selected-value");
+    selectedElement.textContent = optionElement.textContent;
+  }
+
+  //제이쿼리로 슬라이드 토글효과 만들기
+  $(document).ready(function(){
+    const langBtn = $(".selected");
+    const optionBtn = $(".option");
+
+    //언어버튼 클릭시 슬라이드 토글
+    langBtn.click(function(){
+      $(".util ul").slideToggle();
+    });
+    optionBtn.click(function(){
+      $(".util ul").slideUp();
+    })
+
+    //화면 밖 여백 클릭시 토글 닫기
+    $('html').click(function(e){
+      //특정 부모의 자식이 아닐경우 이벤트 실행
+      if($(e.target).parents('.select').length < 1){
+        //console.log('슬라이드 밖이다')
+        $(".util ul").slideUp();
+      }
+
+    })
+  })
+  
+
+
 // ---------- Initialize Swiper ------------
 
 //메인 슬라이드
@@ -58,15 +132,24 @@ let gnb_h = $('.gnb_box').outerHeight();
     //마우스 오버시 배경색,폰트컬러 변경 함수
     $(header).hover(function(){
       $(header).css('background','#fff')
-      $('.gnb > li > a').css('color','var(--text_color_1)')
       $('.lnb > li > a').css('opacity','1')
     },function(){
-      $(header).css('background','rgba(0,0,0,0.2)')
-      $('.gnb > li > a').css('color','#fff')
+      $(header).css('background','rgba(255,255,255,0.5)')
       $('.lnb > li > a').css('opacity','0.3')
     })
 
   });
+
+
+//------------gnb 호버시 서브메뉴 배경색-------------
+$(document).ready(function(){
+  $('.gnb > li').hover(function(){
+    $(this).siblings().css('background','none');
+    $(this).css({'background-image':'url(https://www.color-hex.com/palettes/7431.png)','background-position-y':'100px','background-repeat':'no-repeat'});
+  },function(){
+    $(this).css('background','none');
+  })
+})
 
 
 //-------------계열사 무한슬라이드------------
@@ -90,12 +173,33 @@ let gnb_h = $('.gnb_box').outerHeight();
 
   // 마우스가 요소 위로 진입했을 때 일시정지
   $('.wrap .rolling-list li').hover(function(){
+    //애니메이션 정지
     $('.rolling-list.original').css('animation-play-state', 'paused');
     $('.rolling-list.clone').css('animation-play-state', 'paused');
-  },function(){
+
+    //계열사 버튼 삽입하는 것
+    let group_name = $(this).find('img').attr('alt');
+
+    $(this).append("<div class='group_btn'><p>"+group_name+"</p><div><i class='fas fa-angle-right'></i></div></div>");
+
+  },function(){  //마우스 뗐을때
     $('.rolling-list.original').css('animation-play-state', 'running');
     $('.rolling-list.clone').css('animation-play-state', 'running');
+
+    $(this).find('.group_btn').remove();
+
   });
+
+
+
+//------------인재채용 마우스 오버시 버튼 나오게------------
+$(document).ready(function(){
+  $('.recruit_list li').hover(function(){
+    $(this).find('.recruit_btn').stop().show()
+  },function(){
+    $(this).find('.recruit_btn').stop().hide()
+  })
+})
 
 
 
@@ -120,130 +224,6 @@ $(document).ready(function(){
 })
 
 
-//------------헤더 좌우스크롤시 보여지게 하기----------
-$(document).ready(function(){
-
-  //새로고침시 헤더위치 변경
-  let bwLeft = $(document).scrollLeft();
-  $('.h_inner').css('left',`-${bwLeft}px`)
-
-  //스크롤 할때마다 자동으로 헤더위치 변경
-  $(window).scroll(function(){
-    bwLeft = $(document).scrollLeft();
-    $('.h_inner').css('left',`-${bwLeft}px`)
-  })
-
-})
-
-
-
-//----------------풀페이지 스크롤 함수---------------------
-/* $(document).ready(function(){
-
-  window.addEventListener("wheel", function(e){
-    e.preventDefault();
-  },{passive : false}); //기본 휠 이벤트 제거(?)
-
-  $html = $('html');
-  $html.animate({scrollTop:0},10);
-
-
-	fullset();
-	quickClick();
-  function fullset(){
-    var pageindex = $("#fullpage > .fullsection").size(); //fullpage 안에 섹션이(.fullsection) 몇개인지 확인하기
-  
-    for(var i=1;i<=pageindex;i++){
-      $("#fullpage > .quick > ul").append("<li></li>");
-    }
-    $("#fullpage .quick ul :first-child").addClass("on"); //일단 화면이 로드 되었을때는 퀵버튼에 1번째에 불이 들어오게
-    //마우스 휠 이벤트
-    $(window).on("mousewheel", function(event){
-      var page = $(".quick ul li.on");
-      //alert(page.index()+1);  // 현재 on 되어있는 페이지 번호
-  
-      if($html.is(":animated")) return false;
-  
-      //if($("main").find("#fullpage:animated").length >= 1) return false;
-      //마우스 휠을 위로
-      if(event.originalEvent.wheelDelta >= 0) {
-        var before=page.index();
-        if(page.index() >= 0) page.prev().addClass("on").siblings(".on").removeClass("on");//퀵버튼옮기기
-        var pagelength=0;
-        for(var i=1; i<(before); i++)
-        {
-          pagelength += $(".full"+i).height();
-          console.log(pagelength)
-        }
-        if(page.index() > 0){ //첫번째 페이지가 아닐때 (index는 0부터 시작임)
-          page=page.index()-1;
-          $("#fullpage").animate({"top": -pagelength + "px"},1000, "swing");
-        }else{
-          alert("첫번째페이지 입니다.");
-        }	
-      }else{ // 마우스 휠을 아래로	
-        var nextPage=parseInt(page.index()+1); //다음페이지번호
-        var lastPageNum=parseInt($(".quick ul li").size()); //마지막 페이지번호
-        //현재페이지번호 <= (마지막 페이지 번호 - 1)
-        //이럴때 퀵버튼옮기기
-        if(page.index() <= $(".quick ul li").size()-1){ 
-          page.next().addClass("on").siblings(".on").removeClass("on");
-        }
-        
-        if(nextPage < lastPageNum){ //마지막 페이지가 아닐때만 animate !
-          var pagelength=0;
-          for(var i = 1; i<(nextPage+1); i++){ 
-            //총 페이지 길이 구하기
-            //ex) 현재 1번페이지에서 2번페이지로 내려갈때는 1번페이지 길이 + 2번페이지 길이가 더해짐
-            pagelength += $(".full"+i).height();
-          }
-          $("#fullpage").animate({"top": -pagelength + "px"},1000, "swing");
-        }
-        else{ // 현재 마지막 페이지 일때는
-          alert("마지막 페이지 입니다!");
-        };		
-        
-      }
-    });
-    $(window).resize(function(){ 
-      //페이지가 100%이기때문에 브라우저가 resize 될때마다 스크롤 위치가 그대로 남아있는것을 방지하기 위해
-      var resizeindex = $(".quick ul li.on").index()+1;
-      
-      var pagelength=0;
-      for(var i = 1; i<resizeindex; i++){ 
-        //총 페이지 길이 구하기
-        //ex) 현재 1번페이지에서 2번페이지로 내려갈때는 1번페이지 길이 + 2번페이지 길이가 더해짐
-        pagelength += $(".full"+i).height();
-      }
-  
-      $("#fullpage").animate({"top": -pagelength + "px"},0);
-    });
-  }
-  // 사이드 퀵버튼 클릭 이동
-  function quickClick(){
-    $(".quick li").click(function(){
-      var gnbindex = $(this).index()+1;
-      var length=0;
-      for(var i=1; i<(gnbindex); i++)
-      {
-        length+=$(".full"+i).height();
-      }
-      if($("body").find("#fullpage:animated").length >= 1) return false;
-      $(this).addClass("on").siblings("li").removeClass("on");
-      
-      $("#fullpage").animate({"top": -length + "px"},800, "swing");
-      return false;
-    });
-  }
-}); */
-
-
-
-
-
-
-
-
 // ------------- 풀페이지 스크롤 ---------------
 
 //자바스크립트로 기본 스크롤 패시브 효과 제거
@@ -260,10 +240,17 @@ $html.animate({scrollTop:0},10);
 
 //스크롤 하는 함수
 $(window).on("wheel", function(e){
+  //처음페이지랑 마지막페이지에는 퀵버튼 안보이게
   if(page==1||page==lastPage){
     $('.quick').fadeOut(300)
   }else{
     $('.quick').fadeIn(300)
+  }
+  //처음페이지에는 탑버튼 안보이게
+  if(page==1){
+    $('.t_btn').fadeOut(300)
+  }else{
+    $('.t_btn').fadeIn(300)
   }
   
   //스크롤시 버튼 색상 변경
@@ -273,21 +260,50 @@ $(window).on("wheel", function(e){
   if($html.is(":animated"))
   return;
 
+
+  //스크롤할때 페이지 이동
   if(e.originalEvent.deltaY > 0){
     if(page == lastPage)
     return;
-    page++;
-
-    }else if(e.originalEvent.deltaY < 0){
-      if(page == 1)
-      return;
-      page--;
+    //아래로 스크롤할때 페이지 2~7번째 헤더 색 하얗게 만들기
+    if(page==1||page==2||page==3||page==4||page==5||page==6){
+      $(header).css({'background':'#fff','border-bottom':'1px solid #ccc'})
+      $('.lnb > li > a').css('opacity','1')
     }
+    //페이지 번호 증가
+    page++;
+  }else if(e.originalEvent.deltaY < 0){
+    if(page == 1)
+    return;
+    page--;
+    //페이지를 위로 올릴때(첫페이지로 갈때) 헤더색 투명하게
+    if(page==1){
+      $(header).css({'background':'rgba(255,255,255,0.5)','border-bottom':'none'})
+      $('.lnb > li > a').css('opacity','0.3')
+    }
+  }
 
-    var posTop = (page-1) * $(window).height();
-    $html.animate({scrollTop : posTop},500);
+  //페이지 위치 이동하는 부분
+  var posTop = (page-1) * $(window).height();
+  $html.animate({scrollTop : posTop},500);
+
+  //탑버튼 클릭시 맨위로 이동하고
+  $('.t_btn').click(function(){
+    $html.animate({scrollTop : 0},500);
+    page = 1;
+    //퀵버튼 숨김
+    $('.quick').fadeOut(300);
+  })
+
+  //페이지가 2~7일때 마우스 오버시 헤더 하얗게하기
+  $(header).hover(function(){
+    if(page==2||page==3||page==4||page==5||page==6||page==7){
+      $(header).css('background','#fff')
+      $('.lnb > li > a').css('opacity','1')
+    }
+  })
+
 });
-
 
 
 //-----------------퀵버튼 항상 보이게 해놓기-----------
@@ -310,6 +326,11 @@ $(".quick li").click(function(){
 
   var posTop = (pageNum) * $(window).height();
   $html.animate({scrollTop : posTop},500);
+
+  //페이지 넘버값+1을 다시 page에 전달함
+  page=pageNum+1;
 })
+
+
 
 
